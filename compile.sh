a8218b1c88f6a9ed011b9ecbf16acaa919d2c89f
root <rc.rohan09@live.com>
2019-06-14 14:42:59 +0530

    Adding First Commit

---
+++ compile.sh
@@ -0,0 +1,6 @@
+SOURCE_HOME=/home/rohan/MyGitty/src
+DEBUG_HOME=/home/rohan/MyGitty/Debug
+
+
+g++ --std=c++11 -I${SOURCE_HOME}/HEADER_FILES ${SOURCE_HOME}/SOURCE_FILES/main.cpp -o ${DEBUG_HOME}/Wave2Mp3.o
+
